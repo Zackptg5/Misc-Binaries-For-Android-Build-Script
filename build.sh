1d10fd80b403b93afcf72af2de516554f93e6bd0
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2020-09-15 22:17:14 -0400

    update stuff

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ usage () {
   echogreen "BIN=      (Default: all) (Valid options are: htop, patchelf, strace, vim, zsh, zstd)"
   echogreen "ARCH=     (Default: all) (Valid Arch values: all, arm, arm64, aarch64, x86, i686, x64, x86_64)"
   echogreen "STATIC=   (Default: true) (Valid options are: true, false)"
-  echogreen "API=      (Default: 29) (Valid options are: 21, 22, 23, 24, 26, 27, 28, 29)"
+  echogreen "API=      (Default: 30) (Valid options are: 21, 22, 23, 24, 26, 27, 28, 29, 30)"
   echogreen "          Note that Zsh requires API of 24 or higher for gdbm"
   echogreen "           Note that you can put as many of these as you want together as long as they're comma separated"
   echogreen "           Ex: BIN=htop,vim,zsh"
@@ -136,7 +136,7 @@ TEXTRESET=$(tput sgr0)
 TEXTGREEN=$(tput setaf 2)
 TEXTRED=$(tput setaf 1)
 DIR=$PWD
-NDKVER=r21
+NDKVER=r21d
 STATIC=true
 OIFS=$IFS; IFS=\|;
 while true; do
@@ -152,9 +152,10 @@ IFS=$OIFS
 [ -z "$BIN" -o "$BIN" == "all" ] && BIN="htop patchelf strace vim zsh"
 
 case $API in
-  21|22|23|24|26|27|28|29) ;;
-  *) API=29;;
+  21|22|23|24|26|27|28|29|30) ;;
+  *) API=30;;
 esac
+# MinAPI for htop is 25
 
 if [ -f /proc/cpuinfo ]; then
   JOBS=$(grep flags /proc/cpuinfo | wc -l)
@@ -184,17 +185,17 @@ for i in ar as ld ranlib strip clang gcc clang++ g++; do
   ln -sf $ANDROID_TOOLCHAIN/i686-linux-android-$i $ANDROID_TOOLCHAIN/i686-linux-gnu-$i
 done
 
-NVER=6.1
+NVER=6.2
 PVER=8.43
 ZVER=1.2.11
 for LBIN in $BIN; do
   case $LBIN in
-    "htop") VER="2.2.0"; URL="hishamhm/htop";;
+    "htop") VER="3.0.1"; URL="htop-dev/htop/";;
     "patchelf") VER="0.10"; URL="NixOS/patchelf";;
     "strace") VER="v5.5"; URL="strace/strace";;
     "vim") unset VER; URL="vim/vim";;
-    "zsh") VER="5.7.1";;
-    "zstd") VER="v1.4.4"; URL="facebook/zstd";;
+    "zsh") VER="5.8";;
+    "zstd") VER="v1.4.5"; URL="facebook/zstd";;
     *) echored "Invalid binary specified!"; usage;;
   esac
 
@@ -257,7 +258,10 @@ for LBIN in $BIN; do
         $FLAGS--prefix=$PREFIX
         ;;
       "strace")
-        [ "$LARCH" == "x86_64" ] && FLAGS="--enable-mpers=m32 $FLAGS"
+        case $LARCH in
+          "x86_64") FLAGS="--enable-mpers=m32 $FLAGS";;
+          # "aarch64") FLAGS="--enable-mpers=mx32 $FLAGS";; #mpers-m32 errors since v5.6, uncomment this if using compiling strace 5.6+
+        esac
         ./configure CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" --host=$target_host --target=$target_host \
         $FLAGS--prefix=$PREFIX
         ;;
@@ -326,7 +330,7 @@ for LBIN in $BIN; do
     [ $? -eq 0 ] || { echored "Build failed!"; exit 1; }
     if [ "$LBIN" == "zsh" ]; then
       make install -j$JOBS DESTDIR=$PREFIX
-      ! $STATIC && [ "$LARCH" == "arm64-v8a" -o "$LARCH" == "x86_64" ] && mv -f $DEST/$LARCH/lib $DEST/$LARCH/lib64
+      ! $STATIC && [ "$LARCH" == "aarch64" -o "$LARCH" == "x86_64" ] && mv -f $DEST/$LARCH/lib $DEST/$LARCH/lib64
     else
       make install -j$JOBS
     fi
